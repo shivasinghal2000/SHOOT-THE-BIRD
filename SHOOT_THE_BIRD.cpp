9564d961d3b9702400d024ec2013eeb0f6120e1a
Shiva Singhal <singhalshiva936@gmail.com>
2020-07-06 02:01:09 +0530

    Update SHOOT_THE_BIRD.cpp

--- SHOOT_THE_BIRD.cpp
+++ SHOOT_THE_BIRD.cpp
@@ -45,6 +45,45 @@ void cls()
     Position.Y = 0;
     SetConsoleCursorPosition(hOut, Position);
 }
+
+// clearScreen() and cls() both functions do the same work so anyone of them can be used
+void ClearScreen()
+  {
+  HANDLE                     hStdOut;
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  DWORD                      count;
+  DWORD                      cellCount;
+  COORD                      homeCoords = { 0, 0 };
+
+  hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
+  if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+  /* Get the number of cells in the current buffer */
+  if (!GetConsoleScreenBufferInfo( hStdOut, &csbi )) return;
+  cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+  /* Fill the entire buffer with spaces */
+  if (!FillConsoleOutputCharacter(
+    hStdOut,
+    (TCHAR) ' ',
+    cellCount,
+    homeCoords,
+    &count
+    )) return;
+
+  /* Fill the entire buffer with the current colors and attributes */
+  if (!FillConsoleOutputAttribute(
+    hStdOut,
+    csbi.wAttributes,
+    cellCount,
+    homeCoords,
+    &count
+    )) return;
+
+  /* Move the cursor home */
+  SetConsoleCursorPosition( hStdOut, homeCoords );
+  }
+
 void game::Levels() //Just minimize this function
 ////////////////////////////////////////////////////////////////////////////*//
 {
